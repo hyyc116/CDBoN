6799cd168f8bfc0d5837ef724294dd8157dcc823
hyyc116 <hyyc116@gmail.com>
2017-04-17 08:34:57 -0400

    build citation network

---
+++ Citation_friction.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+import sys
+import json
+from collections import defaultdict
+def build_citation_network(path):
+    ref_dict=defaultdict(dict)
+    data = json.loads(open(path).read())
+    reflist = data['RECORDS']
+    print 'Size of reference', len(reflist)
+    count=0
+    for ref in reflist:
+        cpimt+=1
+        if count%1000==1:
+            print count
+        pid = ref['cpid']
+        pid_year = ref['cpid_year']
+        cited_pid = ref['pid']
+        cited_pid_year = ref['pid_year']
+
+        cited_dict = ref_dict.get(cited_pid,{})
+        cited_dict['pid'] = cited_pid
+        cited_dict['year'] = cited_pid_year
+        citation_list = cited_dict.get('citations',[])
+        citation_list.append('{:},{:}'.format(pid,pid_year))
+        cited_dict['citations']=citation_list
+        ref_dict[cited_pid] = cited_dict
+
+    open('data/aminer_citation_dict.json','w').write(json.dumps(ref_dict))
+    print 'done'
+
+
+if __name__ == '__main__':
+    build_citation_network(sys.argv[1])
