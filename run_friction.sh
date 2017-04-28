3bfc9a2530ee54137ae02837dabec5e929117b94
hyyc116 <hyyc116@gmail.com>
2017-04-27 22:30:58 -0400

    add

--- run_friction.sh
+++ run_friction.sh
@@ -24,6 +24,6 @@
 ### plot cy_cyi_dyi
 # python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_cyi_dyi all
 
-python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_yi_dcyi all
+# python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_yi_dcyi all
 
-
+python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_delta_yi all
