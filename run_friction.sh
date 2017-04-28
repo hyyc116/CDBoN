bb1864c8b7761589e0416fb8d746c7fbd2fb7644
hyyc116 <hyyc116@gmail.com>
2017-04-27 20:53:53 -0400

    add

--- run_friction.sh
+++ run_friction.sh
@@ -1,7 +1,9 @@
-# python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_ti_all
+###### plot co_ti_i
+# python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_ti_i all
 
-python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_ti_i 10
+##### plot co_delta_ti
+python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_delta_ti 10
 
-python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_ti_i 100
+python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_delta_ti 100
 
-python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_ti_i all
+python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json co_delta_ti all
