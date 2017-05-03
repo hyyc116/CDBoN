9a4fd5316638a2a09ca44c4c8cf426511dd523f6
hyyc116 <hyyc116@gmail.com>
2017-05-02 20:35:21 -0400

    add

--- cascades/cascade_statistics.py
+++ cascades/cascade_statistics.py
@@ -132,7 +132,7 @@ def cascade_size_distribution(citation_cascade):
     plt.boxplot(all_data,showfliers=False)
     # ax3.set_xticks([i for i in np.arange(len(bucket_dict.keys()),500)])
     # ax3.set_xticklabels([sorted_keys[i] for i in np.arange(len(bucket_dict.keys()),500)])
-    ax3.set_title('Cascade Size vs. Citation Count')
+    plt.title('Cascade Size vs. Citation Count')
 
 
     plt.tight_layout()
