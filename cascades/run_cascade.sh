db0f783363b21a6c7051f4b08777ecac57dba2b7
hyyc116 <hyyc116@gmail.com>
2017-05-03 22:03:18 -0400

    add

--- cascades/run_cascade.sh
+++ cascades/run_cascade.sh
@@ -1,7 +1,18 @@
 
 ## build cascade tree
-python cascade_statistics.py build_cascade data/aminer_citation_dict.json
+# python cascade_statistics.py build_cascade data/aminer_citation_dict.json
 
+## cascade size distribution
 # python cascade_statistics.py cascade_size data/aminer_citation_cascade.json
 
+## Cascade depth distribution
 # python cascade_statistics.py cascade_depth data/aminer_citation_cascade.json
+
+
+## Cascade subgraphs
+Cascade cascade_statistics.py subgraphs data/aminer_citation_cascade.json
+
+
+
+
+
