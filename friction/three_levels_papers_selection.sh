ed13d9e85d83d5ef984300e02890d6a7cd983df7
hyyc116 <hyyc116@gmail.com>
2017-05-16 21:43:45 -0400

    add

--- friction/three_levels_papers_selection.sh
+++ friction/three_levels_papers_selection.sh
@@ -12,7 +12,7 @@
 
 
 ## plot timely speed / cy_cyi_yi
-python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_cyi_yi all
+python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_delta_cyi_yi all
 
 ## plot average speed / cy_cyi_dyi
 python Citation_friction.py co_three_levels data/low_selected_papers.json data/medium_selected_papers.json data/high_selected_papers.json cy_cyi_dyi all
