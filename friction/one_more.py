b0ccc4fb052417a005b3fde1319c9abb84a52dab
hyyc116 <hyyc116@gmail.com>
2017-05-18 14:17:05 -0400

    add

--- friction/one_more.py
+++ friction/one_more.py
@@ -1,20 +1,6 @@
 from para_config import *
 
 
-#from perspective of citation order
-def citation_order(cited_papers_json,xyfunc=co_ti_i,i='all'):
-    cited_papers = json.loads(open(cited_papers_json).read())
-    xs_ys_dict={}
-    
-    for k in cited_papers.keys():
-        paper_dict = cited_papers[k]
-        pid = paper_dict['pid']
-        year = paper_dict['year']
-        citations = [(cit.split(',')[0],int(cit.split(',')[1])) for cit in paper_dict['citations']]
-        xs,ys = xyfunc(citations,year,i)
-        xs_ys_dict[pid]=(xs,ys)
-
-    return xs_ys_dict
 
 def first_citation(cited_papers_json):
     cited_papers = json.loads(open(cited_papers_json).read())
@@ -35,8 +21,11 @@ def first_citation(cited_papers_json):
 
 def plot_three_level_first_citations(low,medium,high):
     # fig,axes = plt.subplots(1,3,figszie=(15,5))
+    logging.info('plot first citations....')
 
     # ax1 = axes[0]
+    logging.info('Low cited papers ...')
+
     internal_dict = first_citation(low)
     xs = []
     ys = []
@@ -46,6 +35,7 @@ def plot_three_level_first_citations(low,medium,high):
     ys = np.array(ys)/float(sum(ys))
     plt.plot(xs,ys,'low cited papers')
 
+    logging.info('Medium cited papers ...')
     internal_dict = first_citation(medium)
     xs = []
     ys = []
@@ -55,6 +45,7 @@ def plot_three_level_first_citations(low,medium,high):
     ys = np.array(ys)/float(sum(ys))
     plt.plot(xs,ys,'medium cited papers')
 
+    logging.info('High cited papers ...')
     internal_dict = first_citation(high)
     xs = []
     ys = []
@@ -65,6 +56,8 @@ def plot_three_level_first_citations(low,medium,high):
     plt.plot(xs,ys,'high cited papers')
 
     plt.savefig('pdf/first_citation.pdf',dpi=300)
+
+    logging.info('saved to pdf/first_citation.pdf')
 
 if __name__ == '__main__':
     plot_three_level_first_citations(sys.argv[1],sys.argv[2],sys.argv[3])
