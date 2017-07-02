6155c7f81a3c0131507c697386f0555f4e629802
hyyc116 <hyyc116@gmail.com>
2017-07-02 16:34:47 -0400

    add

--- cascades/run_subgraph.sh
+++ cascades/run_subgraph.sh
@@ -1,7 +1,7 @@
 
 #subgraph 
-for i in {2..15}
+for i in {2..20}
 do
     echo ${i}
-    python cascade_statistics.py gen_sub ${i}> 'subN/subs_${i}.txt'
+    python cascade_statistics.py gen_sub ${i} 1>'subN/subs_'${i}'.txt' 2>'subN/log_'${i}'.txt' 
 done
