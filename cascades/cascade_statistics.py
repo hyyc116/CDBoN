74e52391cec86c2cbf362d9bcfe0c0d202934211
hyyc116 <hyyc116@gmail.com>
2017-08-04 09:36:22 -0400

    add

--- cascades/cascade_statistics.py
+++ cascades/cascade_statistics.py
@@ -178,7 +178,7 @@ def stats_plot():
     ax3=axes[2]
     xs=[]
     ys=[]
-    for depth in sorted([i for i in depth_dict.keys()]):
+    for depth in sorted([int(i) for i in depth_dict.keys()]):
         xs.append(int(depth))
         ys.append(depth_dict[depth])
 
