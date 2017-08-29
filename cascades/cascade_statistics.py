312103d396c43e3da283cf1ac0c09237af7d70db
hyyc116 <hyyc116@gmail.com>
2017-08-28 21:57:04 -0400

    add

--- cascades/cascade_statistics.py
+++ cascades/cascade_statistics.py
@@ -533,7 +533,8 @@ def plot_dict():
         fit_y.append(max_dict[key])
 
     popt,pcov = curve_fit(square_x,fit_x,fit_y) 
-    ax2.plot(cxs, square_x(cxs, *popt))
+
+    ax2.plot(np.array(cxs), square_x(np.array(cxs), *popt))
     ax2.scatter(cxs,rys)
     ax2.set_xlabel('Citation Count\n(b)')
     ax2.set_ylabel('Average Marginal Value')
