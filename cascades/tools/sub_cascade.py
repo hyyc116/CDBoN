0a2b0a0ef49c3f9d5af1f3b218d768d462383d5e
hyyc116 <hyyc116@gmail.com>
2017-09-04 12:26:27 -0400

    add

--- cascades/tools/sub_cascade.py
+++ cascades/tools/sub_cascade.py
@@ -273,7 +273,7 @@ def plot_size_n(ax,size_dict,n):
         ys.append(avg_percent)
 
     # ax.plot(xs,ys,c=color_sequence[n-1],label='N={:}'.format(n))
-    z = zip(*lowess(ys,xs,frac= 0.1))[1]
+    z = zip(*lowess(ys,np.log(np.array(xs)),frac= 0.1))[1]
     ax.plot(xs,z,label='size = {:}'.format(n),c=color_sequence[n-1])
 
 if __name__ == '__main__':
