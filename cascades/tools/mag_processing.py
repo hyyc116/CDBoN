abcae729e23695e1a241f9dd65cd5b7b3507217a
hyyc116 <hyyc116@gmail.com>
2017-09-18 22:30:59 -0400

    add

--- cascades/tools/mag_processing.py
+++ cascades/tools/mag_processing.py
@@ -3,25 +3,21 @@ from basic_config import *
 
 def build_reference_network(dirpath):
 
-    citation_network=defaultdict(list)
-    paper_year = defaultdict(int)
+    
     file_index = 0
     line_index = 0
     for file in os.listdir(dirpath):
         file_index+=1
         filepath = dirpath[:-1] if dirpath.endswith('/') else dirpath
         filepath=filepath+"/"+file
+
+        citation_network=defaultdict(list)
+        paper_year = defaultdict(int)
         for line in open(filepath):
             line_index+=1
 
             if line_index%10000==0:
                 logging.info('The {:} th File:{:}, total progress:{:}'.format(file_index,filepath,line_index))
-
-
-            if line_index%1000000==100000:
-                open('data/mag_citation_network.json','w').write(json.dumps(citation_network))
-                open('data/mag_paper_year.json','w').write(json.dumps(paper_year))
-                logging.info('save json, file index: {:}, line_index:{:}'.format(file_index,line_index))
 
             line = line.strip()
             paper = json.loads(line)
@@ -34,9 +30,9 @@ def build_reference_network(dirpath):
                 for cpid in paper['references']:
                     citation_network[cpid].append(pid)
 
-    open('data/mag_citation_network.json','w').write(json.dumps(citation_network))
-    open('data/mag_paper_year.json','w').write(json.dumps(paper_year))
-    logging.info('save json, file index: {:}, line_index:{:}'.format(file_index,line_index))
+        open('data/mag/mag_citation_network_{:}.json'.format(file_index),'w').write(json.dumps(citation_network))
+        open('data/mag/mag_paper_year_{:}.json'.format(file_index),'w').write(json.dumps(paper_year))
+        logging.info('save json, file index: {:}, line_index:{:}'.format(file_index,line_index))
 
 
 if __name__ == '__main__':
