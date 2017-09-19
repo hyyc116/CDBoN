ccf6b20730328e7a032c94266224080218e61b4b
hyyc116 <hyyc116@gmail.com>
2017-09-19 10:12:58 -0400

    add

--- cascades/tools/mag_processing.py
+++ cascades/tools/mag_processing.py
@@ -34,7 +34,38 @@ def build_reference_network(dirpath):
         open('data/mag/mag_paper_year_{:}.json'.format(file_index),'w').write(json.dumps(paper_year))
         logging.info('save json, file index: {:}, line_index:{:}'.format(file_index,line_index))
 
+def merger_dict(dirpath,prefix,t='list'):
+    if t =='list':
+        merger_dict = defaultdict(list)
+    else:
+        merger_dict = defaultdict(int)
+
+    for file in os.listdir(dirpath):
+        if not file.startswith(prefix):
+            continue
+        filepath = dirpath[:-1] if dirpath.endswith('/') else dirpath
+        filepath=filepath+"/"+file
+
+        data = json.loads(open(filepath).read())
+
+        for key in data.keys():
+            if t=='list':
+                for v in data[k]:
+                    merger_dict[key].append(v)
+            else:
+                merger_dict[key] = data[k]
+    if t=='list':
+        outpath = 'data/mag_citation_network.json'
+    else:
+        outpath = 'data/mag_paper_year.json'
+
+    open('outpath','w').write(merger_dict)
+
+
+
+
 
 if __name__ == '__main__':
-    build_reference_network(sys.argv[1])
+    # build_reference_network(sys.argv[1])
+    merger_dict(sys.argv[1],sys.argv[2],sys.argv[3])
 
