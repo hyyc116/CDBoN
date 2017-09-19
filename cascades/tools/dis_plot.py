c0058599b8efac6cb7f0b8f17d4679bde2069901
hyyc116 <hyyc116@gmail.com>
2017-09-19 12:17:43 -0400

    add

--- cascades/tools/dis_plot.py
+++ cascades/tools/dis_plot.py
@@ -190,7 +190,7 @@ def stats_plot():
 
 
     popt,pcov = curve_fit(power_low_func,xs[:10],ys[:10])
-    ax4.plot(np.linspace(1, 10, 10), power_low_func(np.linspace(1, 10, 10), *popt)*10,label='$\\alpha={:.2f}$'.format(popt[0]),c=color_sequence[2])
+    ax4.plot(np.linspace(1, 10, 10), power_low_func(np.linspace(1, 10, 10), *popt)*10,label='$\\alpha={:.2f}$'.format(popt[0]),c=color_sequence[3])
 
     ax4.set_title('degree distribution')
     ax4.set_yscale('log')
@@ -230,7 +230,7 @@ def stats_plot():
     ax5.plot(xs,ys,'o',fillstyle='none',label='Out-degree',c='r')
 
 
-    ax5.plot(np.linspace(10, 30, 10), power_low_func(np.linspace(10, 30, 10), *popt)/10,label='$\\alpha={:.2f}$'.format(popt[0]),c=color_sequence[2])
+    ax5.plot(np.linspace(10, 30, 10), power_low_func(np.linspace(10, 30, 10), *popt)/10,label='$\\alpha={:.2f}$'.format(popt[0]),c=color_sequence[4])
     # ax5.plot([_80_x]*10,np.linspace(_min_y,_max_y,10),'--',label='$x={:}$'.format(_80_x))
 
     # ax5.set_title('out degree distribution')
