19682fa9f7777bb4cc9ecb8b6047e539b2bca691
hyyc116 <hyyc116@gmail.com>
2017-10-02 20:38:52 -0400

    add

--- cascades/tools/basic_config.py
+++ cascades/tools/basic_config.py
@@ -111,7 +111,7 @@ def plot_heat_scatter(xs,ys,ax,fig):
     print zs[:10],max(zs)
     print len(xs),len(ys),len(zs)
     norm = mpl.colors.LogNorm(vmin=min(zs),vmax=max(zs))
-    ax.scatter(xs, ys, c=CM.winter(norm(zs)), marker='o',s=4)
+    ax.scatter(xs, ys, c=CM.winter(norm(zs)), marker='o')
 
     colmap = CM.ScalarMappable(norm=norm, cmap=CM.winter)
     colmap.set_array(zs)
