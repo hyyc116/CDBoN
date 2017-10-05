71caf285bf43ee34fabad82eb3871b99129fe235
hyyc116 <hyyc116@gmail.com>
2017-10-05 10:07:08 -0400

    add

--- friction/citation_nums.py
+++ friction/citation_nums.py
@@ -112,11 +112,11 @@ def paras_square(xs,ys):
 
     fig,axes = plt.subplots(12,8,figsize=(40,60))
     for i,start in enumerate([5,10,15,20,21,22,23,24,25,30,40,50]):
-        for j,end in enumerate([200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000]):
+        for j,maxc in enumerate([200,300,400,500,600,700,800,900,1000,2000,3000,4000,5000]):
 
 
             for xi,v in enumerate(xs):
-                if v>j:
+                if v>maxc:
                     end = xi-1
                     break
 
