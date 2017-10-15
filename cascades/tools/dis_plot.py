d9eae0801d63241e9ccde7be62d539713e777c4d
hyyc116 <hyyc116@gmail.com>
2017-10-15 16:45:35 -0400

    add

--- cascades/tools/dis_plot.py
+++ cascades/tools/dis_plot.py
@@ -101,6 +101,12 @@ def stats_plot():
     ax2.set_title('cascade size distribution')
     ax2.set_xlabel('$x=$cascade size\n(b)')
     ax2.set_ylabel('$N_{size}(x)$')
+
+    mean = np.sum(np.array(xs[25:1060])*np.array(ys[25:1060])/np.sum(ys[25:1060]))
+    ax2.plot([mean]*10,np.linspace(_min_y,_max_y,10),'--',label='mean={:.2f}'.format(mean))
+    logging.info('-- mean -- {:}'.format(mean))
+
+
     ax2.set_yscale('log')
     ax2.set_xscale('log') 
     ax2.legend()
