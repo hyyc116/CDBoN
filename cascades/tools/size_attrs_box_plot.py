901ba7a742141c9cb74ad7422f68f3ddfaaac71e
hyyc116 <hyyc116@gmail.com>
2017-10-20 00:41:53 -0400

    add

--- cascades/tools/size_attrs_box_plot.py
+++ cascades/tools/size_attrs_box_plot.py
@@ -40,8 +40,9 @@ def plot_relation_size_attr(dataset):
 
     ## 对 n_direct_citations 是一个比例列表
     # 使用最大的值进行归一化
-    normed_direct_cps = np.array(n_direct_citations)/np.max(n_direct_citations)
 
+    normed_direct_cps = np.log(np.array(n_direct_citations)/np.max(n_direct_citations))
+    normed_direct_cps = [10**np.round(i) for i in normed_direct_cps]
     direct_citation_size_dict = defaultdict(list)
     for i,depth in enumerate(dys):
         # cascade 的大小
